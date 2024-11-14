60cc79b9f0c08c665df41c7be8365fd83bdc51fe
swparabellum <swparabellum@gmail.com>
2024-11-15 00:00:25 +0900

    2-05 ing

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,29 @@
 package com.mysite.sbb;
 
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private QuestionRepository questionRepository;
 
+
+    @Test
+    void testJpa() {
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2, all.size());
+
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 }
